91cbcc1bbc55131ad3c9d5d1b772e0ff1a39e795
Absurdistbeezay <b_jay05@hotmail.com>
2018-03-11 04:33:04 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$('.nav-head').click(function(){
+    $('.collapse').collapse('hide');
+});
+
+$(".center").slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: false,
+    nextArrow: false
+  });
+
