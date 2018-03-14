acf8793f8588a1e3ab0abcc4ba649456f51409ba
Absurdistbeezay <b_jay05@hotmail.com>
2018-03-14 00:56:26 -0400

    sections updated

---
+++ public/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.nav-head').click(function(){
+        $('.collapse').collapse('hide');
+    });
+    
+    $(".center").slick({
+            slidesToShow: 7,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            prevArrow: false,
+            nextArrow: false
+          });
+
+ 
+});
+
+  
+
